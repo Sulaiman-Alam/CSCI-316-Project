ef111f3fd1b4817565315972c40f1f80b8b79789
Sulaiman Alam <sanewyork1931@gmail.com>
2025-04-14 20:40:28 -0400

    file can be read now

--- project.java
+++ project.java
@@ -1,5 +1,21 @@
+import java.util.*;
+import java.io.*;
+
 public class project {
     public static void main(String args[]) {
+        try {
+            File myObj = new File("Code.txt");
+            Scanner myReader = new Scanner(myObj);
 
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                // Will add a check to make sure job is correct if time permits
+                System.out.println(line);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
